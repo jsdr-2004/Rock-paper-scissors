8f2b4904adf66e0e81fe9b0d7726e15e8e20d7f4
jsdr-2004 <89971768+jsdr-2004@users.noreply.github.com>
2021-10-19 16:15:25 +0530

    ROCK_PAPER_SCISSORS
    Mark 3

--- code.py
+++ code.py
@@ -1,22 +1,26 @@
 import random
-uc = input("enter your choice('rock','paper','scissors')")
-pc = ['rock','paper','scissors']
-cc = random.choice(pc)
-print(f"your choice is {uc},computer's choice is {cc}")
-if uc == cc:
-    print(f"Both players selected {uc}. It's a tie!")
-elif uc == "rock":
-    if cc == "scissors":
-        print("You win!")
-    else:
-        print("You lose.")
-elif uc == "paper":
-    if cc == "rock":
-        print(" You win!")
-    else:
-        print(" You lose.")
-elif uc == "scissors":
-    if cc == "paper":
-        print("You win!")
-    else:
-        print("You lose.")
+x = int(input('how tries do you need?'))
+for i in range(x):
+    uc = input("enter your choice('rock','paper','scissors')")
+    pc = ['rock','paper','scissors']
+    cc = random.choice(pc)
+    print(f"your choice is {uc},computer's choice is {cc}")
+    if uc == cc:
+        print(f"Both players selected {uc}. It's a tie!")
+    elif uc == "rock":
+        if cc == "scissors":
+            print("You win!")
+        else:
+            print("You lose.")
+    elif uc == "paper":
+        if cc == "rock":
+            print(" You win!")
+        else:
+            print(" You lose.")
+    elif uc == "scissors":
+        if cc == "paper":
+            print("You win!")
+        else:
+            print("You lose.")
+i=+1
+print('thanks for playing the game')
